badd1129dc77391fa756565f9f0a1c97919c26dd
paul <paul.s.falke@gmail.com>
2020-12-12 12:51:55 +0100

    book_store_final

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -2,8 +2,8 @@ from collections import Counter
 
 disc_prices = (0, 800, 1520, 2160, 2560, 3000)
 
-def total(basket):
 
+def total(basket):
     tot_price = 0
     books = Counter(basket)
     book_number = list()
@@ -11,13 +11,10 @@ def total(basket):
     while len(books) > 0:
         tot_price += disc_prices[len(books)]
         book_number.append(len(books))
-
+        
         books = {i: j-1 for i, j in books.items()}
         books = {i: j for i, j in books.items() if j != 0}
 
-    while 3 in book_number and 5 in book_number:
-        tot_price -= 40
-        book_number.remove(3)
-        book_number.remove(5)
+    discount = 40 * min(book_number.count(3), book_number.count(5))
 
-    return tot_price
+    return tot_price - discount 
