5db00da91fd0a566c15f14ac8a340785b7a1bd08
paulsok <paul.s.falke@gmail.com>
2021-09-12 20:06:05 +0200

    making the grade

---
+++ python/making-the-grade/loops.py
@@ -0,0 +1,57 @@
+def round_scores(student_scores):
+    """
+    :param student_scores: list of student exam scores as float or int.
+    :return: list of student scores *rounded* to nearest integer value.
+    """
+
+    return [round(x) for x in student_scores]
+
+
+def count_failed_students(student_scores):
+    """
+    :param student_scores: list of integer student scores.
+    :return: integer count of student scores at or below 40.
+    """
+
+    return len([x for x in student_scores if x <= 40])
+
+
+def above_threshold(student_scores, threshold):
+    """
+    :param student_scores: list of integer scores
+    :param threshold :  integer
+    :return: list of integer scores that are at or above the "best" threshold.
+    """
+
+    return [x for x in student_scores if x >= threshold]
+
+
+def letter_grades(highest):
+    """
+    :param highest: integer of highest exam score.
+    :return: list of integer score thresholds for each F-A letter grades.
+    """
+
+    assert highest > 40
+
+    return list(range(41, highest, (highest-40)//4))
+
+
+def student_ranking(student_scores, student_names):
+    """
+     :param student_scores: list of scores in descending order.
+     :param student_names: list of names in descending order by exam score.
+     :return: list of strings in format ["<rank>. <student name>: <score>"].
+     """
+
+    return [f'{i + 1}. {student_names[i]}: {student_scores[i]}'
+            for i in range(len(student_scores))]
+
+
+def perfect_score(student_info):
+    """
+    :param student_info: list of [<student name>, <score>] lists
+    :return: First [<student name>, 100] found OR "No perfect score."
+    """
+    return next(([n, s] for n, s in student_info if s == 100),
+                'No perfect score.')
