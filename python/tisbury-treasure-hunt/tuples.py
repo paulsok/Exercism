ab5a32ae823d0660d6465d61d617c0fb7a09ad12
paulsok <paul.s.falke@gmail.com>
2021-09-12 21:23:18 +0200

    tisbury treasure hunt

---
+++ python/tisbury-treasure-hunt/tuples.py
@@ -0,0 +1,58 @@
+def get_coordinate(record):
+    """
+
+    :param record: tuple - a (treasure, coordinate) pair.
+    :return: str - the extracted map coordinate.
+    """
+
+    return record[1]
+
+
+def convert_coordinate(coordinate):
+    """
+
+    :param coordinate: str - a string map coordinate
+    :return:  tuple - the string coordinate seperated into its individual components.
+    """
+
+    return tuple(coordinate)
+
+
+def compare_records(azara_record, rui_record):
+    """
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return: bool - True if coordinates match, False otherwise.
+    """
+
+    return convert_coordinate(get_coordinate(azara_record)) == rui_record[1]
+
+
+def create_record(azara_record, rui_record):
+    """
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return:  tuple - combined record, or "not a match" if the records are incompatible.
+    """
+
+    if not compare_records(azara_record, rui_record):
+        return 'not a match'
+
+    return azara_record + rui_record
+
+
+def clean_up(combined_record_group):
+    """
+
+    :param combined_record_group: tuple of tuples - everything from both participants.
+    :return: tuple of tuples - everything "cleaned". Excess coordinates and information removed.
+    """
+
+    result = ''
+    for record in combined_record_group:
+        rep_tuple = (record[0],) + record[2:]
+        result += f'{rep_tuple}\n'
+
+    return result
