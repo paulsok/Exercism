98c45335ce8efe3cfc202e9147b9645e48e19122
paulsok <paul.s.falke@gmail.com>
2021-09-28 22:52:34 +0200

    yacht

---
+++ python/yacht/yacht.py
@@ -0,0 +1,64 @@
+"""
+This exercise stub and the test suite contain several enumerated constants.
+
+Since Python 2 does not have the enum module, the idiomatic way to write
+enumerated constants has traditionally been a NAME assigned to an arbitrary,
+but unique value. An integer is traditionally used because it’s memory
+efficient.
+It is a common practice to export both constants and functions that work with
+those constants (ex. the constants in the os, subprocess and re modules).
+
+You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
+"""
+
+
+# Score categories.
+# Change the values as you see fit.
+YACHT = "yacht"
+ONES = "ones"
+TWOS = "twos"
+THREES = "threes"
+FOURS = "fours"
+FIVES = "fives"
+SIXES = "sixes"
+FULL_HOUSE = "full_house"
+FOUR_OF_A_KIND = "four_kind"
+LITTLE_STRAIGHT = "little_straight"
+BIG_STRAIGHT = "big_straight"
+CHOICE = "choice"
+
+
+def score(dice, category):
+    if category == ONES:
+        return dice.count(1) * 1
+    elif category == TWOS:
+        return dice.count(2) * 2
+    elif category == THREES:
+        return dice.count(3) * 3
+    elif category == FOURS:
+        return dice.count(4) * 4
+    elif category == FIVES:
+        return dice.count(5) * 5
+    elif category == SIXES:
+        return dice.count(6) * 6
+    elif category == FULL_HOUSE:
+        if len(set(dice)) == 2 and dice.count(dice[0]) in [2, 3]:
+            return sum(dice)
+        return 0
+
+    elif category == FOUR_OF_A_KIND:
+        for die in dice[:2]:
+            if dice.count(die) >= 4:
+                return die * 4
+        return 0
+
+    elif category == LITTLE_STRAIGHT:
+        return 30 if sorted(dice) == [1, 2, 3, 4, 5] else 0
+    elif category == BIG_STRAIGHT:
+        return 30 if sorted(dice) == [2, 3, 4, 5, 6] else 0
+    elif category == CHOICE:
+        return sum(dice)
+    elif category == YACHT:
+        return 50 if len(set(dice)) == 1 else 0
+
+    return 0
