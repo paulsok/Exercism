1207f641b611cc5efa78d57dde59a694f7fd73bb
paulsok <paul.s.falke@gmail.com>
2021-09-30 23:07:54 +0200

    prime factors

---
+++ python/prime-factors/prime_factors.py
@@ -0,0 +1,9 @@
+def factors(value):
+    factors, n = [], 2
+    while value > 1:
+        while value % n == 0:
+            factors.append(n)
+            value /= n
+        n += 1
+
+    return factors
