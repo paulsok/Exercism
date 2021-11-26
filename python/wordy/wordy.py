311cac79eed2ca45fb56e486c46c987da26fa8ad
paulsok <paul.s.falke@gmail.com>
2021-11-26 23:35:49 +0100

    wordy

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,3 +1,7 @@
+def is_valid_number(string):
+    return string.isdecimal() or string[0] == '-' and string[1:].isdecimal()
+
+
 def answer(question):
     if question == "":
         raise ValueError("No question, really?")
@@ -12,4 +16,40 @@ def answer(question):
         raise ValueError("unknown operation")
     if not is_valid_number(words[2]):
         raise ValueError("syntax error")
-    return None
+
+    result = int(words[2])
+    index = 3
+
+    while index < len(words):
+        if index + 1 < len(words) and words[index] in ["plus", "minus"] and is_valid_number(words[index+1]):
+            if words[index] == "plus":
+                result += int(words[index+1])
+            else:
+                result -= int(words[index+1])
+            index += 2
+
+        elif index + 2 < len(words) and words[index] in ["multiplied", "divided"] and words[index+1] == "by" and is_valid_number(words[index+2]):
+            if words[index] == "multiplied":
+                result *= int(words[index+2])
+            else:
+                result /= int(words[index+2])
+            index += 3
+
+        elif index + 4 < len(words) and words[index:index+3] == ["raised", "to", "the"] and len(words[index+3]) > 2\
+                                    and words[index+3][-2:] in ["th", "st", "nd"] and is_valid_number(words[index+3][:-2]) and words[index+4] == 'power':
+            result **= int(words[index+3][:-2])
+            index += 5
+
+        elif words[index] in ["plus", "minus", "multiplied", "divided", "raised"]:
+            if index + 1 < len(words) and words[index+1] in ["plus", "minus", "multiplied", "divided", "raised"] and words[index] != words[index+1]:
+                raise ValueError("unknown operation")
+            raise ValueError("syntax error")
+
+        else:
+            if is_valid_number(words[index]):
+                if index == 3 and index + 2 < len(words) and is_valid_number(words[index+2]):
+                    raise ValueError("unknown operation")
+                raise ValueError("syntax error")
+            raise ValueError("unknown operation")
+
+    return result
