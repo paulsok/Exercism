6e1dc3b4d56f6b8403de04cb57d51d7d51cbca8a
paulsok <paul.s.falke@gmail.com>
2021-12-14 22:35:26 +0100

    robot name

---
+++ python/robot-name/robot_name.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+
+class Robot:
+    def __init__(self):
+        self.name = self.name_me()
+
+    def reset(self):
+        self.name = self.name_me()
+
+    def name_me(self):
+        random.seed()
+        letters = random.choices(string.ascii_uppercase, k=2)
+        numbers = random.choices(string.digits, k=3)
+        return ''.join(letters + numbers)
