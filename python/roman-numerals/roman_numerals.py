6c5198bbd9eba6d855e4992311a4e00b69e72014
paulsok <paul.s.falke@gmail.com>
2022-01-21 22:09:05 +0100

    roman numerals

---
+++ python/roman-numerals/roman_numerals.py
@@ -0,0 +1,26 @@
+numbs_map = {1000: "M",
+                900: "CM",
+                500: "D",
+                400: "CD",
+                100: "C",
+                90: "XC",
+                50: "L",
+                40: "XL",
+                10: "X",
+                9: "IX",
+                5: "V",
+                4: "IV",
+                1: "I"}
+
+
+def roman(number):
+    if number < 0 or number > 3999:
+        raise ValueError("Value must be in the range 0 - 3999")
+    result = ''
+
+    for value, numeral in numbs_map.items():
+        while number >= value:
+            number -= value
+            result += numeral
+
+    return result
